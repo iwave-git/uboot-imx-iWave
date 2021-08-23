0cf8168fdda14c46c5f4b6be18e43e64f2236daa
Rakshith <support.ip@iwavesystems.com>
2021-08-23 12:13:44 +0530

    Updating UUU BSP Version
    Signed-off-by: Rakshith <support.ip@iwavesystems.com>

--- include/configs/imx8mp_iwg40m_uuu.h
+++ include/configs/imx8mp_iwg40m_uuu.h
@@ -4,14 +4,14 @@
  * SPDX-License-Identifier:     GPL-2.0+
  */
 
-#ifndef __IMX8MN_IWG40M_UUU_H
-#define __IMX8MN_IWG40M_UUU_H
+#ifndef __IMX8MP_IWG40M_UUU_H
+#define __IMX8MP_IWG40M_UUU_H
 
 #include "imx8mp_iwg40m.h"
 
 #ifdef BSP_VERSION
 #undef BSP_VERSION
-#define BSP_VERSION     "iW-PRGQZ-SC-01-R1.0-REL0.1-Linux5.4.70_UUU"
+#define BSP_VERSION     "iW-PRGQZ-SC-01-R2.0-REL0.1-Linux5.4.70_UUU"
 #endif
 
 #endif
